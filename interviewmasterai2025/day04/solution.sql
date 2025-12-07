94645bf5be0d996b095741b1b263c45a5b5d9154
siberian goddess <46657597+siberiangoddess@users.noreply.github.com>
2025-12-07 21:14:01 +0300

    Create solution.sql

---
+++ interviewmasterai2025/day04/solution.sql
@@ -0,0 +1,11 @@
+-- Energy-Efficient Holiday Decorations
+-- Kevin's trying to decorate the house without sending the electricity
+-- bill through the roof. Write a query to find the top 5 most
+-- energy-efficient decorations (i.e. lowest cost per hour to operate).
+
+select 
+  decoration_id
+  , decoration_name
+from hall_decorations
+order by energy_cost_per_hour
+limit 5
