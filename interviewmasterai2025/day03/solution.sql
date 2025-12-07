1e7c4866e7888d5a84174a184ad441d6272e2715
siberian goddess <46657597+siberiangoddess@users.noreply.github.com>
2025-12-07 21:19:51 +0300

    Update solution.sql

--- interviewmasterai2025/day03/solution.sql
+++ interviewmasterai2025/day03/solution.sql
@@ -1,8 +1,8 @@
--- The Grinch's Best Pranks Per Target
--- The Grinch has brainstormed a ton of pranks for Whoville, but he only wants
--- to keep the top prank per target, with the highest evilness score. Return
--- the most evil prank for each target. If two pranks have the same evilness,
--- the more recently brainstormed wins.
+/* The Grinch's Best Pranks Per Target
+The Grinch has brainstormed a ton of pranks for Whoville, but he only wants
+to keep the top prank per target, with the highest evilness score. Return
+the most evil prank for each target. If two pranks have the same evilness,
+the more recently brainstormed wins. */
 
 select 
   target_name
