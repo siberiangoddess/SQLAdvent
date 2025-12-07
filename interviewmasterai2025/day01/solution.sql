1c4e8b2fcc264b172ed84b59b9c2967b8a3a53f2
siberian goddess <46657597+siberiangoddess@users.noreply.github.com>
2025-12-07 21:09:41 +0300

    Create solution.sql

---
+++ interviewmasterai2025/day01/solution.sql
@@ -0,0 +1,8 @@
+-- Reindeer Run - Find the Top 7
+-- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh.
+-- Write a query to return the name and rank of the top 7 reindeers in this race.
+
+select name, rank
+from reindeer_run_results
+order by rank
+limit 7
